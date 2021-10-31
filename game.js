6a0f7ca608f49592071afa2ff938ef6309be42f5
kevinjd0 <91098523+kevinjd0@users.noreply.github.com>
2021-11-01 02:55:33 +0400

    Added function makeBees()
    Added function makeBees() according to the number of bees specified by the user
    Updated start() function to create bees in an array to keep track of number of bees

--- game.js
+++ game.js
@@ -47,6 +47,11 @@ function start() {
     document.addEventListener("keydown", moveBear, false);
     //Added an event listener to change speed of bear according to user input
     document.addEventListener("onChange",setSpeed,false);
+    
+    //create new array for bees
+    bees = new Array();
+    //create bees
+    makeBees();
 }
 
 // Handle keyboad events 
@@ -157,4 +162,25 @@ function createBeeImg(wNum) {
 
     //return the img object
     return img;
-   }
+}
+
+function makeBees() {
+    //get number of bees specified by the user
+    let nbBees = document.getElementById("nbBees").value;
+    nbBees = Number(nbBees); //try converting the content of the input to a number 
+    
+    if (isNaN(nbBees)) { //check that the input field contains a valid number
+    window.alert("Invalid number of bees");
+    return;
+    }
+
+    //create bees 
+    let i = 1;
+    while (i <= nbBees) {
+        var num = i;
+        var bee = new Bee(num); //create object and its IMG element
+        bee.display(); //display the bee
+        bees.push(bee); //add the bee object to the bees array
+        i++;
+    }
+}
