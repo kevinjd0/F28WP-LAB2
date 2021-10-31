eb69815ab8ccf043fe79584578f1b9bd8acd622c
kevinjd0 <91098523+kevinjd0@users.noreply.github.com>
2021-11-01 02:12:47 +0400

    Added class bee
    Class has attributes of the bee IMG element and methods to move the bees and display bees

--- game.js
+++ game.js
@@ -70,3 +70,51 @@ function moveBear(e) {
     } // down key
 
 }
+
+class Bee {
+    constructor(beeNumber) {
+
+        //the HTML element corresponding to the IMG of the bee
+        this.htmlElement = createBeeImg(beeNumber);
+        //iits HTML ID
+        this.id = this.htmlElement.id;
+        //the left position (x)
+        this.x = this.htmlElement.offsetLeft;
+        //the top position (y)
+        this.y = this.htmlElement.offsetTop;
+
+        this.move = function(dx, dy) {
+            //move the bees by dx, dy
+            this.x += dx;
+            this.y += dy;
+            this.display();
+        };
+
+        this.display = function() {
+            //adjust position of bee and display it
+            this.fitBounds();//add this to adjust to bounds
+            this.htmlElement.style.left = this.x + "px";
+            this.htmlElement.style.top = this.y + "px";
+            this.htmlElement.style.display = "block";
+        };
+
+        this.fitBounds = function() {
+            //check and make sure the bees stays in the board space
+            let parent = this.htmlElement.parentElement;
+            let iw = this.htmlElement.offsetWidth;
+            let ih = this.htmlElement.offsetHeight;
+            let l = parent.offsetLeft;
+            let t = parent.offsetTop;
+            let w = parent.offsetWidth;
+            let h = parent.offsetHeight;
+            if (this.x < 0)
+            this.x = 0;
+            if (this.x > w - iw)
+            this.x = w - iw;
+            if (this.y < 0)
+            this.y = 0;
+            if (this.y > h - ih)
+            this.y = h - ih;
+            };
+    }
+}
