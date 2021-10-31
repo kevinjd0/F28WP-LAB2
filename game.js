6778676577cc50d6191daf46c1d868bbb8133e2f
kevinjd0 <91098523+kevinjd0@users.noreply.github.com>
2021-11-01 02:01:51 +0400

    Added function to handle keyboard events and move bear
    Added function to direct movements of bear according to key pressed (up,down,left,right)
    Added event listener in start() function

--- game.js
+++ game.js
@@ -23,4 +23,34 @@ function Bear() {
 function start() {
     //create bear
     bear = new Bear();
-   }
+
+    // Add an event listener to the keypress event.
+    document.addEventListener("keydown", moveBear, false);
+}
+
+// Handle keyboad events 
+// to move the bear
+function moveBear(e) {
+    //codes of the four keys
+    const KEYUP = 38;
+    const KEYDOWN = 40;
+    const KEYLEFT = 37;
+    const KEYRIGHT = 39;
+
+    if (e.keyCode == KEYRIGHT) {
+    bear.move(1, 0)
+    } // right key
+
+    if (e.keyCode == KEYLEFT) {
+    bear.move(-1, 0)
+    } // left key
+
+    if (e.keyCode == KEYUP) {
+    bear.move(0, -1)
+    } // up key
+
+    if (e.keyCode == KEYDOWN) {
+    bear.move(0, 1)
+    } // down key
+
+}
